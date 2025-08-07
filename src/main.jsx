7b93ee96dacebf9d5d1b55a775e84af1d0497d60
unknown <irina.bojkovska.ib@gmail.com>
2025-08-07 22:31:19 +0200

    fixed translation a third time today

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import "./i18n.js";
 createRoot(document.getElementById("root")).render(
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense
+    fallback={
+      <div className="h-screen bg-black text-green-300 flex justify-center items-center">
+        <p>Loading...</p>
+      </div>
+    }
+  >
     <App />
   </Suspense>
 );
